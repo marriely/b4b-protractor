3e9af4d7cf5ac2879912bed80a552b270e9f8ec0
Marriely <marriely@hotmail.com>
2019-11-19 16:23:22 -0300

    altera

--- spec/preview.js
+++ spec/preview.js
@@ -8,24 +8,29 @@ describe('Executar preview', function() {
         console.log("passando pelo beforeEach");
         preview = new Preview();
         login = new Login();
-        browser.driver.get('https://teste.botfactory.newwaycorp.io'); 
+        browser.driver.get('https://teste.botfactory.newwaycorp.io/'); 
+        browser.driver.manage().window().maximize();
         browser.ignoreSynchronization = true;
 
            
-        login.email.sendKeys('qateste@teste.com.br');
+        login.email.sendKeys('novo@teste.com.br');
         login.senha.sendKeys('123456');
         login.btn_entrar.click();
         console.log("Passou pelo login sucesso");
 
     });
 
-    it('Executar o preview', function() {  
-
-        var EC = protractor.ExpectedConditions;
-        // Waits for the element with id 'abc' to be clickable.
-      
-        browser.wait(EC.elementToBeClickable($(preview.novo_fluxo)), 5000);
+    it('Novo fluxo', function() {  
         
+        browser.sleep(5000);
+        //expect(browser.getCurrentUrl()).toBe('https://teste.botfactory.newwaycorp.io/flows/');
+
+    //  preview.novo_fluxo.click();
+       
+    element.all(by.className('[data-test=flowNewButton]')).click();
+       console.log("passou pelo it")
+       
+        //clicou no novo fluxo
   
   }); // fim do preview
 
